3ba53ba67f7488aec849b65661f62ad26204d70a
Yosthim Enciso <a20201497@pucp.edu.pe>
2024-04-25 19:24:03 -0500

    Proyecto creado

---
+++ src/main/java/com/example/GTICSLAB420201497/GticsLab420201497Application.java
@@ -0,0 +1,13 @@
+package com.example.GTICSLAB420201497;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GticsLab420201497Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GticsLab420201497Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GTICSLAB420201497/GticsLab420201497ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GTICSLAB420201497;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GticsLab420201497ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
